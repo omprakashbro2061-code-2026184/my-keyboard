96d1a8d605dfd9925f3b269711b6562a0c526fb8
omprakashbro2061-code-2026184 <omprakashbro2061@gmail.com>
2026-04-19 01:41:04 +0545

    Update MyKeyboardService.kt

--- app/src/main/java/com/example/keyboard/MyKeyboardService.kt
+++ app/src/main/java/com/example/keyboard/MyKeyboardService.kt
@@ -1,21 +1,29 @@
 package com.example.keyboard
 
 import android.inputmethodservice.InputMethodService
-import android.inputmethodservice.KeyboardView
 import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView
 import android.view.View
 
 class MyKeyboardService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
 
     private lateinit var keyboardView: KeyboardView
-    private lateinit var keyboard: Keyboard
+    private lateinit var lettersKeyboard: Keyboard
+    private lateinit var symbolsKeyboard: Keyboard
     private var isCaps = false
     private var isSymbols = false
 
+    companion object {
+        const val CODE_SPACE = 32
+        const val CODE_TOGGLE = 100500
+        const val CODE_SYMBOLS2 = 100501
+    }
+
     override fun onCreateInputView(): View {
         keyboardView = layoutInflater.inflate(R.layout.keyboard_view, null) as KeyboardView
-        keyboard = Keyboard(this, R.xml.keys_letters)
-        keyboardView.keyboard = keyboard
+        lettersKeyboard = Keyboard(this, R.xml.keys_letters)
+        symbolsKeyboard = Keyboard(this, R.xml.keys_symbols)
+        keyboardView.keyboard = lettersKeyboard
         keyboardView.setOnKeyboardActionListener(this)
         keyboardView.isPreviewEnabled = false
         return keyboardView
@@ -23,37 +31,54 @@ class MyKeyboardService : InputMethodService(), KeyboardView.OnKeyboardActionLis
 
     override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
         val ic = currentInputConnection ?: return
+
         when (primaryCode) {
+
             Keyboard.KEYCODE_DELETE -> {
-                ic.deleteSurroundingText(1, 0)
+                val selectedText = ic.getSelectedText(0)
+                if (selectedText.isNullOrEmpty()) {
+                    ic.deleteSurroundingText(1, 0)
+                } else {
+                    ic.commitText("", 1)
+                }
             }
+
             Keyboard.KEYCODE_SHIFT -> {
                 isCaps = !isCaps
-                keyboard.isShifted = isCaps
+                lettersKeyboard.isShifted = isCaps
                 keyboardView.invalidateAllKeys()
             }
+
             Keyboard.KEYCODE_DONE -> {
                 ic.commitText("\n", 1)
             }
-            32 -> {
+
+            CODE_SPACE -> {
                 ic.commitText(" ", 1)
             }
-            100500 -> {
-                isSymbols = !isSymbols
-                keyboard = if (isSymbols) {
-                    Keyboard(this, R.xml.keys_symbols)
-                } else {
-                    Keyboard(this, R.xml.keys_letters)
-                }
-                keyboardView.keyboard = keyboard
+
+            CODE_TOGGLE -> {
+                isSymbols = true
+                keyboardView.keyboard = symbolsKeyboard
             }
+
+            CODE_SYMBOLS2 -> {
+                isSymbols = false
+                keyboardView.keyboard = lettersKeyboard
+                isCaps = false
+                lettersKeyboard.isShifted = false
+                keyboardView.invalidateAllKeys()
+            }
+
             else -> {
-                var code = primaryCode.toChar()
-                if (isCaps) code = code.uppercaseChar()
-                ic.commitText(code.toString(), 1)
-                if (isCaps) {
+                var char = primaryCode.toChar()
+                if (!isSymbols && isCaps) {
+                    char = char.uppercaseChar()
+                }
+                ic.commitText(char.toString(), 1)
+                if (!isSymbols && isCaps) {
                     isCaps = false
-                    keyboard.isShifted = false
+                    lettersKeyboard.isShifted = false
                     keyboardView.invalidateAllKeys()
                 }
             }
