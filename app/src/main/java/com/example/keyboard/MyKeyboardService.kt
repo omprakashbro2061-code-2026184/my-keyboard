7b105c5e02fa5911d524b372128cc7732dad705d
omprakashbro2061-code-2026184 <omprakashbro2061@gmail.com>
2026-04-18 20:23:48 +0545

    Create MyKeyboardService.kt

---
+++ app/src/main/java/com/example/keyboard/MyKeyboardService.kt
@@ -0,0 +1,72 @@
+package com.example.keyboard
+
+import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.KeyboardView
+import android.inputmethodservice.Keyboard
+import android.view.View
+import android.view.inputmethod.EditorInfo
+
+class MyKeyboardService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+
+    private lateinit var keyboardView: KeyboardView
+    private lateinit var keyboard: Keyboard
+    private var isCaps = false
+    private var isSymbols = false
+
+    override fun onCreateInputView(): View {
+        keyboardView = layoutInflater.inflate(R.layout.keyboard_view, null) as KeyboardView
+        keyboard = Keyboard(this, R.xml.keys_letters)
+        keyboardView.keyboard = keyboard
+        keyboardView.setOnKeyboardActionListener(this)
+        keyboardView.isPreviewEnabled = false
+        return keyboardView
+    }
+
+    override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
+        val ic = currentInputConnection ?: return
+
+        when (primaryCode) {
+            Keyboard.KEYCODE_DELETE -> {
+                ic.deleteSurroundingText(1, 0)
+            }
+            Keyboard.KEYCODE_SHIFT -> {
+                isCaps = !isCaps
+                keyboard.isShifted = isCaps
+                keyboardView.invalidateAllKeys()
+            }
+            Keyboard.KEYCODE_DONE -> {
+                ic.performEditorAction(EditorInfo.IME_ACTION_DONE)
+            }
+            -2 -> {
+                isSymbols = !isSymbols
+                keyboard = if (isSymbols) {
+                    Keyboard(this, R.xml.keys_symbols)
+                } else {
+                    Keyboard(this, R.xml.keys_letters)
+                }
+                keyboardView.keyboard = keyboard
+            }
+            -3 -> {
+                ic.commitText(" ", 1)
+            }
+            else -> {
+                var code = primaryCode.toChar()
+                if (isCaps) code = code.uppercaseChar()
+                ic.commitText(code.toString(), 1)
+                if (isCaps) {
+                    isCaps = false
+                    keyboard.isShifted = false
+                    keyboardView.invalidateAllKeys()
+                }
+            }
+        }
+    }
+
+    override fun onPress(primaryCode: Int) {}
+    override fun onRelease(primaryCode: Int) {}
+    override fun onText(text: CharSequence?) {}
+    override fun swipeDown() {}
+    override fun swipeLeft() {}
+    override fun swipeRight() {}
+    override fun swipeUp() {}
+}
